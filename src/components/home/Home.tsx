1af28fb49021be082cff546447c574e51aab77d8
gio797 <g.muchaidze@yahoo.com>
2023-10-11 14:53:16 +0400

    setup home page layout

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -16,44 +16,49 @@ type Props = {
 function Home({ setSelectedPage }: Props) {
   const isAboveMediumScreen = useMediaQuery("(min-width:1060px");
   return (
-    <section
-      id="home"
-      className={`home ${isAboveMediumScreen && "padding-wide-screen"}`}
-    >
-      <div className={`${isAboveMediumScreen && "flex"}`}>
-        <div className="left-section">
-          <div className="headers">
-            <img src={HomePageText} alt="home-page-text" />
-            <p>
-              Unrivaled Gym. Unparalleled Training Fitness Classes. World Class
-              Studios to get the Body Shapes That you Dream of.. Get Your Dream
-              Body Now.
-            </p>
+    <section id="home">
+      <div className="section">
+        <div
+          className={`title ${
+            isAboveMediumScreen ? "flex padding-wide-screen" : "flex-column"
+          }`}
+        >
+          <div className="left-section">
+            <div className="headers">
+              <div className="img-wrapper">
+                <img src={HomePageText} alt="home-page-text" />
+              </div>
+              <p>
+                Unrivaled Gym. Unparalleled Training Fitness Classes. World
+                Class Studios to get the Body Shapes That you Dream of.. Get
+                Your Dream Body Now.
+              </p>
+            </div>
+            <div className="actions">
+              <ActionButton setSelectedPage={setSelectedPage}>
+                Join Now
+              </ActionButton>
+              <AnchorLink
+                className="link"
+                onClick={() => setSelectedPage(SelectedPage.ContactUs)}
+                href={`#${SelectedPage.ContactUs}`}
+              >
+                <p>Learn More</p>
+              </AnchorLink>
+            </div>
           </div>
-          <div className="actions">
-            <ActionButton setSelectedPage={setSelectedPage}>
-              Join Now
-            </ActionButton>
-            <AnchorLink
-              className="link"
-              onClick={() => setSelectedPage(SelectedPage.ContactUs)}
-              href={`#${SelectedPage.ContactUs}`}
-            >
-              <p>Learn More</p>
-            </AnchorLink>
+          <div className="right-section">
+            <img src={HomePageGraphic} alt="home-page-graphic" />
           </div>
         </div>
-        <div className="right-section">
-          <img src={HomePageGraphic} alt="home-page-graphic" />
-        </div>
+        {isAboveMediumScreen && (
+          <div className="sponsors padding-wide-screen">
+            <img src={SponsorRedBull} alt="red-bull" />
+            <img src={SponsorForbes} alt="forbes" />
+            <img src={SponsorFortune} alt="fortune" />
+          </div>
+        )}
       </div>
-      {isAboveMediumScreen && (
-        <div>
-          <img src={SponsorRedBull} alt="red-bull" />
-          <img src={SponsorForbes} alt="forbes" />
-          <img src={SponsorFortune} alt="fortune" />
-        </div>
-      )}
     </section>
   );
 }
