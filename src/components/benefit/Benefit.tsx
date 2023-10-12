3c3b0b3f8ed8f5629ca2d0e8ae78ad43104f6f21
gio797 <g.muchaidze@yahoo.com>
2023-10-12 14:58:38 +0400

    finish benefits page

--- src/components/benefit/Benefit.tsx
+++ src/components/benefit/Benefit.tsx
@@ -1,6 +1,12 @@
 import AnchorLink from "react-anchor-link-smooth-scroll";
 import { SelectedPage } from "../../shared/type";
 import "./benefit.css";
+import { motion } from "framer-motion";
+
+const childVariant = {
+  hidden: { opacity: 0, scale: 0.9 },
+  visible: { opacity: 1, scale: 1 },
+};
 
 type Props = {
   icon: JSX.Element;
@@ -11,7 +17,7 @@ type Props = {
 
 function Benefit({ icon, title, description, setSelectedPage }: Props) {
   return (
-    <div className="benefit">
+    <motion.div variants={childVariant} className="benefit">
       <div className="benefit-circle">{icon}</div>
       <h4>{title}</h4>
       <p>{description}</p>
@@ -22,7 +28,7 @@ function Benefit({ icon, title, description, setSelectedPage }: Props) {
       >
         <p>Learn More</p>
       </AnchorLink>
-    </div>
+    </motion.div>
   );
 }
 
--- src/components/benefits/Benefits.tsx
+++ src/components/benefits/Benefits.tsx
@@ -4,8 +4,10 @@ import "./benefits.css";
 import HText from "../../shared/hText/HText";
 import { HiAcademicCap, HiOutlineUserGroup } from "react-icons/hi";
 import { AiOutlineHome } from "react-icons/ai";
-import Benefit from "../benefit/benefit";
+import Benefit from "../benefit/Benefit";
 import useMediaQuery from "../../hooks/useMediaQuery";
+import BenefitsGraphic from "../../assets/BenefitsPageGraphic.png";
+import ActionButton from "../../shared/actionButton/ActionButton";
 
 const benefits: Array<BenefitType> = [
   {
@@ -28,6 +30,13 @@ const benefits: Array<BenefitType> = [
   },
 ];
 
+const container = {
+  hidden: {},
+  visible: {
+    transition: { staggerChildren: 0.2 },
+  },
+};
+
 type Props = {
   setSelectedPage: (value: SelectedPage) => void;
 };
@@ -36,22 +45,38 @@ function Benefits({ setSelectedPage }: Props) {
   const isAboveMediumScreen = useMediaQuery("(min-width:1060px");
   return (
     <section
-      id="#benefits"
+      id="benefits"
       className={isAboveMediumScreen ? "benefits-wide-screen" : "benefits"}
     >
       <motion.div
         onViewportEnter={() => setSelectedPage(SelectedPage.Benefits)}
       >
-        <div className="benefits-header">
+        <motion.div
+          className="benefits-header"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, x: -50 },
+            visible: { opacity: 1, x: 0 },
+          }}
+        >
           <HText>MORE THAN JUST GYM.</HText>
           <p className="benefits-desc">
             We provide world class fitness equipment, trainers and classes to
             get you to your ultimate fitness goals with ease. We provide true
             care into each and every member.
           </p>
-        </div>
+        </motion.div>
 
-        <div className="benefits-grid">
+        <motion.div
+          className="benefits-grid"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          variants={container}
+        >
           {benefits.map((benefit: BenefitType) => (
             <Benefit
               key={benefit.title}
@@ -61,6 +86,57 @@ function Benefits({ setSelectedPage }: Props) {
               setSelectedPage={setSelectedPage}
             />
           ))}
+        </motion.div>
+        <div className="benefits-graphics">
+          <img src={BenefitsGraphic} alt="benefits-graphic-image" />
+
+          <div className="benefits-graphics-description">
+            <motion.div
+              className="benefits-graphics-title"
+              initial="hidden"
+              whileInView="visible"
+              viewport={{ once: true, amount: 0.5 }}
+              transition={{ duration: 0.5 }}
+              variants={{
+                hidden: { opacity: 0, x: 50 },
+                visible: { opacity: 1, x: 0 },
+              }}
+            >
+              <HText>
+                MILLIONS OF HAPPY MEMBERS GETTING <span>FIT</span>
+              </HText>
+            </motion.div>
+            <motion.div
+              initial="hidden"
+              whileInView="visible"
+              viewport={{ once: true, amount: 0.5 }}
+              transition={{ delay: 0.2, duration: 0.5 }}
+              variants={{
+                hidden: { opacity: 0, x: 50 },
+                visible: { opacity: 1, x: 0 },
+              }}
+            >
+              <p>
+                Nascetur aenean massa auctor tincidunt. Iaculis potenti amet
+                egestas ultrices consectetur adipiscing ultricies enim. Pulvinar
+                fames vitae vitae quis. Quis amet vulputate tincidunt at in
+                nulla nec. Consequat sed facilisis dui sit egestas ultrices
+                tellus. Ullamcorper arcu id pretium sapien proin integer nisl.
+                Felis orci diam odio.
+              </p>
+              <p>
+                Fringilla a sed at suspendisse ut enim volutpat. Rhoncus vel est
+                tellus quam porttitor. Mauris velit euismod elementum arcu neque
+                facilisi. Amet semper tortor facilisis metus nibh. Rhoncus sit
+                enim mattis odio in risus nunc.
+              </p>
+            </motion.div>
+            <div className="benefits-graphics-btn-wrapper">
+              <ActionButton setSelectedPage={setSelectedPage}>
+                Join Now
+              </ActionButton>
+            </div>
+          </div>
         </div>
       </motion.div>
     </section>
